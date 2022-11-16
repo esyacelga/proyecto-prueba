b025f98c4ec9df0919f633cc98be0362b7e72be3
SY <esyacelga@hotmail.com>
2022-11-16 11:03:44 -0500

    Finalizaciòn pruebas

--- spec/persona-spec.js
+++ spec/persona-spec.js
@@ -4,28 +4,28 @@ const app = require('../index')
 
 describe("Status 200", function () {
     it("Espera que el endpoint raiz este activo", async function () {
-        const response = await request('localhost:3005').get('/');
+        const response = await request('localhost:' + process.env.PORT).get('/');
         expect(response.status).toBe(200);
     });
 });
 
 describe("Get personas", function () {
     it("Espera que la lista no este vacia", async function () {
-        const response = await request('localhost:3005').get('/api/persona');
-        const json =JSON.parse(response.text)
+        const response = await request('localhost:' + process.env.PORT).get('/api/persona');
+        const json = JSON.parse(response.text)
         const objetoLista = json.personas;
         //console.log(objetoLista);
-        const  tamanioLista = objetoLista.length;
+        const tamanioLista = objetoLista.length;
         expect(tamanioLista).toBeGreaterThan(0);
-    },10000);
+    }, 10000);
 });
 
 describe("Obtener persona por ID", function () {
     it("Espera que exista le persona enviada por parametro", async function () {
-        const response = await request('localhost:3005').get('/api/persona/busquedaId/637414dde80978de8c731881');
-        const json =JSON.parse(response.text)
-        expect(json).not.toBeNull()
-    },10000);
+        const response = await request('localhost:' + process.env.PORT).get('/api/persona/busquedaId/63750356181038ec20fdb813');
+        const json = JSON.parse(response.text)
+        expect(json.personaDb).not.toBeUndefined()
+    }, 10000);
 });
 
 describe("Crea una persona", function () {
@@ -37,9 +37,9 @@ describe("Crea una persona", function () {
             telefono: '022873122',
             email: 'martha05@yacelga.com'
         }
-        const response = await request('localhost:3005').post('/api/persona').send(personaNueva);
+        const response = await request('localhost:' + process.env.PORT).post('/api/persona').send(personaNueva);
         expect(response.status).toBe(200);
-    },10000);
+    }, 10000);
 });
 
 describe("Actualiza una persona", function () {
@@ -51,9 +51,9 @@ describe("Actualiza una persona", function () {
             telefono: '022873122',
             email: 'marth0211@yacelga.com'
         }
-        const response = await request('localhost:3005').put('/api/persona/637413da990568b8ff2bd0a0').send(personaNueva);
+        const response = await request('localhost:' + process.env.PORT).put('/api/persona/637413da990568b8ff2bd0a0').send(personaNueva);
         expect(response.status).toBe(200);
-    },10000);
+    }, 10000);
 });
 
 
